e3b22cdadc730573e58b62e9793a8e489b24c059
Elf-mousE <ifmouz@gmail.com>
2019-10-18 20:59:32 +0800

    update

--- 20191018-excel/index.js
+++ 20191018-excel/index.js
@@ -1,53 +1,53 @@
-const path = require('path');
-const Excel = require('exceljs');
+const path = require("path");
+const Excel = require("exceljs");
 
-const oldFilename = path.join(__dirname, 'data/old.xlsx');
-const newFilename = path.join(__dirname, 'data/new.xlsx');
-
-const demo = {
-  text: {
-    richText: [
-      {
-        font: {
-          underline: 'single',
-          color: { argb: 'FFFF6600' },
-          name: '宋体'
-        },
-        text: 'BalmJS'
-      }
-    ]
-  },
-  hyperlink: 'https://balmjs.com/'
-};
+const oldFilename = path.join(__dirname, "data/old.xlsx");
+const newFilename = path.join(__dirname, "data/new.xlsx");
 
 let oldWorkbook = new Excel.Workbook();
 let newWorkbook = new Excel.Workbook();
-let newWorksheet = newWorkbook.addWorksheet('BalmJS');
 
 oldWorkbook.xlsx
   .readFile(oldFilename)
   .then(() => {
-    // Input
     const oldWorksheet = oldWorkbook.getWorksheet(1);
+    let newWorksheet = newWorkbook.addWorksheet("template");
+
+    // Input
     oldWorksheet.eachRow({ includeEmpty: true }, function(row, rowNumber) {
-      console.log('Row ' + rowNumber + ' = ' + JSON.stringify(row.values));
+      // console.log("Row " + rowNumber + " = " + JSON.stringify(row.values));
+
       row.eachCell(function(cell, colNumber) {
-        // console.log('Cell ' + colNumber + ' = ' + cell.value);
-        if (colNumber === 2 && typeof cell.value === 'object') {
-          cell.value = demo;
+        // console.log("Cell " + colNumber + " = " + JSON.stringify(cell.value));
+        if (colNumber === 7 && typeof cell.value === "object") {
+          let hyperlink = cell.value.hyperlink.replace(
+            /AP%20Filling/,
+            "AP%30Filling"
+          );
+          cell.value = {
+            text: cell.value.text,
+            hyperlink
+          };
         }
       });
 
       newWorksheet.addRow(row.values);
     });
 
+    // Test
+    // newWorksheet.eachRow({ includeEmpty: true }, function(row, rowNumber) {
+    //   if (rowNumber < 100) {
+    //     console.log("Row " + rowNumber + " = " + JSON.stringify(row.values));
+    //   }
+    // });
+
     // Output
     newWorkbook.xlsx
       .writeFile(newFilename)
       .then(() => {
         const micros = stopwatch.microseconds;
-        console.log('Done.');
-        console.log('Time taken:', micros);
+        console.log("Done.");
+        console.log("Time taken:", micros);
       })
       .catch(error => {
         console.log(error.message);
